cb5372b09c446e54486667491950fcfc9e49b7c0
Marc S Delahousse <mdelahou@cisco.com>
2019-04-01 13:33:06 -0400

    Initial project structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='firex-bundle-foobar',
+      version='0.1.',
+      description='Example library with fake tasks',
+      url='https://github.com/FireXStuff/firex-bundle-foobar.git',
+      author='Core FireX Team',
+      author_email='firex-dev@gmail.com',
+      license='BSD-3-Clause',
+      packages=['firex_bundle_foobar'],
+      zip_safe=True,
+      install_requires=[
+          "firexapp",
+      ],)
